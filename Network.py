c3c4df561b0983048ab3b0b06eb9013b45500e04
kosc93 <skonrad@student.chalmers.se>
2016-09-13 14:46:59 +0200

    work done   network kind of runs

--- Network.py
+++ Network.py
@@ -1,27 +1,35 @@
 from Neuron import *
 import random as rand
+import numpy as np
+import matplotlib.pyplot as plot
 
 class Network:
     neurons = []
 
     def __init__(self,N,p):
         for i in range(N):
-            n = Neuron(i,N)
-            self.neurons.append(n)
+            self.neurons.append(Neuron(i,N))
         for i in range(len(self.neurons)):
             self.neurons[i].connectHopfield(self.neurons)
 
     def storePatterns(self, patterns):
-        for i in range(len(patterns)):
+        for i in range(patterns.shape[0]):
             for j in range(len(self.neurons)):
                 self.neurons[j].storePattern(patterns[i])
         for i in range(len(self.neurons)):
             self.neurons[i].calcWeights()
 
-    def run(self):
-        i = rand.randint(0,len(self.neurons))
-        self.neurons[i].singleStep()
+    def runHopfield(self, patterns):
+        r= rand.randint(0,patterns.shape[0]-1)
+        for i in range(len(self.neurons)):
+            self.neurons[i].state=patterns[r,i]
+        for i in range(len(self.neurons)):
+            self.neurons[i].singleStep()
 
 if __name__ == '__main__':
-    n= Network(10,1)
-    a = 1
+    N=100
+    p=20
+    patterns=np.random.random_integers(0,1,size=(p,N))
+    n= Network(N,p)
+    n.storePatterns(patterns)
+    a=1
--- Neuron.py
+++ Neuron.py
@@ -2,22 +2,22 @@ import sys
 import numpy as NP
 
 class Neuron:
-    ID = 0 # identifies the neuron
-    inputs = [] # list of connected Neurons
-    weights = [] # list of weights of the Neurons
-    N = 0 # total number of Neurons
-    storedPatterns = []
-    state = 0
+
 
     def __init__(self, ID, N):
         self.ID = ID
         self.N = N
+        self.inputs = []  # list of connected Neurons
+        self.weights = []  # list of weights of the Neurons
+        self.storedPatterns = []
+        self.state = 0
+        self.nextState = 0
 
 
     def connectHopfield(self , Neurons):
         for i in range(len(Neurons)):
             if Neurons[i].ID != self.ID:
-                self.inputs.append(i)
+                self.inputs.append(Neurons[i])
 
     def storePattern(self, pattern):
         if len(pattern)==self.N:
@@ -27,17 +27,18 @@ class Neuron:
 
     def calcWeights(self):
         for i in range(len(self.inputs)):
-            sumResult=0
+            sumResult = 0
             for j in range(len(self.storedPatterns)):
-                sumResult+=self.inputs[i].storedPattern[j]*self.storedPatterns[j]
-            w=1/self.N*sumResult
+                sumResult+=self.inputs[i].storedPatterns[j]*self.storedPatterns[j]
+            w = sumResult*1.0/self.N
             self.weights.append(w)
 
     def singleStep(self):
         sumResult=0
         for i in range(len(self.inputs)):
             sumResult+=self.weights[i]* self.inputs[i].state
-        self.state=NP.sign(sumResult)
+        self.nextState=NP.sign(sumResult)
 
 
-
+    def transferStates(self):
+        self.state=self.nextState
