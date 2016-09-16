bea403e5bd6c4d01813bbeeee2a1f4984ff65362
kosc93 <skonrad@student.chalmers.se>
2016-09-16 11:58:30 +0200

    work done   formated the code

--- Network.py
+++ Network.py
@@ -3,12 +3,16 @@ import random as rand
 import numpy as np
 import matplotlib.pyplot as plot
 
+
+# todo calculate weightmatrix in network and store it in neurons for more flexibility
+
+
 class Network:
     neurons = []
 
-    def __init__(self,N,p):
+    def __init__(self, N, p):
         for i in range(N):
-            self.neurons.append(Neuron(i,N))
+            self.neurons.append(Neuron(i, N))
         for i in range(len(self.neurons)):
             self.neurons[i].connectHopfield(self.neurons)
 
@@ -20,16 +24,17 @@ class Network:
             self.neurons[i].calcWeights()
 
     def runHopfield(self, patterns):
-        r= rand.randint(0,patterns.shape[0]-1)
+        r = rand.randint(0, patterns.shape[0] - 1)
         for i in range(len(self.neurons)):
-            self.neurons[i].state=patterns[r,i]
+            self.neurons[i].state = patterns[r, i]
         for i in range(len(self.neurons)):
             self.neurons[i].singleStep()
 
+
 if __name__ == '__main__':
-    N=100
-    p=20
-    patterns=np.random.random_integers(0,1,size=(p,N))
-    n= Network(N,p)
+    N = 100
+    p = 20
+    patterns = np.random.random_integers(0, 1, size=(p, N))
+    n = Network(N, p)
     n.storePatterns(patterns)
-    a=1
+    a = 1
--- Neuron.py
+++ Neuron.py
@@ -1,9 +1,8 @@
 import sys
 import numpy as NP
 
+
 class Neuron:
-
-
     def __init__(self, ID, N):
         self.ID = ID
         self.N = N
@@ -13,32 +12,30 @@ class Neuron:
         self.state = 0
         self.nextState = 0
 
-
-    def connectHopfield(self , Neurons):
+    def connectHopfield(self, Neurons):
         for i in range(len(Neurons)):
             if Neurons[i].ID != self.ID:
                 self.inputs.append(Neurons[i])
 
     def storePattern(self, pattern):
-        if len(pattern)==self.N:
+        if len(pattern) == self.N:
             self.storedPatterns.append(pattern[self.ID])
         else:
-          sys.exit("stored pattern has an incorrect size")
+            sys.exit("stored pattern has an incorrect size")
 
     def calcWeights(self):
         for i in range(len(self.inputs)):
             sumResult = 0
             for j in range(len(self.storedPatterns)):
-                sumResult+=self.inputs[i].storedPatterns[j]*self.storedPatterns[j]
-            w = sumResult*1.0/self.N
+                sumResult += self.inputs[i].storedPatterns[j] * self.storedPatterns[j]
+            w = sumResult * 1.0 / self.N
             self.weights.append(w)
 
     def singleStep(self):
-        sumResult=0
+        sumResult = 0
         for i in range(len(self.inputs)):
-            sumResult+=self.weights[i]* self.inputs[i].state
-        self.nextState=NP.sign(sumResult)
-
+            sumResult += self.weights[i] * self.inputs[i].state
+        self.nextState = NP.sign(sumResult)
 
     def transferStates(self):
-        self.state=self.nextState
+        self.state = self.nextState
